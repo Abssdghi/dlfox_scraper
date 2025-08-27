2aa243c531571aeae8f5319ff900f565c8eb7acf
Abssdghi <abbas.sadeghi1384@gmail.com>
2025-08-27 23:00:46 +0330

    improve get_game_info func result structure

--- src/dlfox_scraper.py
+++ src/dlfox_scraper.py
@@ -57,6 +57,12 @@ def get_game_info(url="https://dlfox.com/fortnite/"):
         }
     """
     result = {
+        "success": True,
+        "message": "Operation successful",
+        "data": {}
+    }
+    
+    data = {
         "url": "",
         "title": "",
         "size": "",
@@ -79,31 +85,33 @@ def get_game_info(url="https://dlfox.com/fortnite/"):
         content = game.find('div', {'class':'single_content'})
         
     except Exception as e:
+        result["success"] = False
+        result["message"] = e
         return result
 
     # url
-    result['url'] = url
+    data['url'] = url
 
     # title
-    result['title'] = content.find('a',{'class':'mod_articles_category_title'}).text
+    data['title'] = content.find('a',{'class':'mod_articles_category_title'}).text
 
     # size and version and category
     info = content.find('div', {'class':'widget_header'}).find_all('td')
     info_text = []
     for i in info:
         info_text.append(i.text)
-    result['size'] = info_text[3]
-    result['version'] = info_text[-1]
-    result['category'] = info_text[5]
+    data['size'] = info_text[3]
+    data['version'] = info_text[-1]
+    data['category'] = info_text[5]
 
     # desc
-    result['desc'] = content.find_all('p')[2].text
+    data['desc'] = content.find_all('p')[2].text
 
     # system
     try:
-        result['system'] = content.find('div', {'class':'system_text'}).text
+        data['system'] = content.find('div', {'class':'system_text'}).text
     except Exception as e:
-        result['system'] = str(e)
+        data['system'] = str(e)
 
     # images
     images = []
@@ -115,7 +123,7 @@ def get_game_info(url="https://dlfox.com/fortnite/"):
             images.append(i.get('src'))
     except Exception as e:
         images.append(str(e))
-    result['images'] = images
+    data['images'] = images
 
     # download_links
     links = []
@@ -126,8 +134,8 @@ def get_game_info(url="https://dlfox.com/fortnite/"):
             links.append(i.get('href'))
     except Exception as e:
         links.append(str(e))
-    result['download_links'] = links
-    result['parts'] = len(links)
+    data['download_links'] = links
+    data['parts'] = len(links)
 
     # comments
     # comments = []
@@ -138,8 +146,9 @@ def get_game_info(url="https://dlfox.com/fortnite/"):
     #         comments.append(i.get_text()[:len(i)-8])
     # except Exception as e:
     #     comments.append(str(e))  
-    # result['comments'] = comments
+    # data['comments'] = comments
 
+    result["data"] = data
     return result
 
 
